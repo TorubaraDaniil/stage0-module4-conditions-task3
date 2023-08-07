d55e03a0eb7ad7d9a336fca806929824aa97c263
daniil-torubara <dabarara@bk.ru>
2023-08-07 20:14:08 +0600

    module 4 task 3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((int)character >= 65 && (int)character <= 122) {
+            switch (character) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    System.out.println("Vowel");
+                    break;
+                default:
+                    System.out.println("Consonant");
+            }
+        }
+        else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
